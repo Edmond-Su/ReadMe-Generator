0b8cf4a2bd0f21db603eb35a5628671001d41f74
Edmond Su <133119772+deverstater07@users.noreply.github.com>
2023-07-25 18:02:10 +1000

    Added questions and added inquirer prompt to init function in index.js. Started coding fucntions in generateMarkdown.js

--- index.js
+++ index.js
@@ -1,13 +1,71 @@
+const inquirer = require("inquirer");
+const fs = require("fs")
+const generateMarkdown = require("./utils/generateMarkdown.js")
+
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is your GitHub username",
+        name: "github",
+    },
+    {
+        type: "input",
+        message: "What is your email address",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "What is your project's name",
+        name: "projectName",
+    },
+    {
+        type: "input",
+        message: "Please write a short description of your project.",
+        name: "desc",
+    },
+    {
+        type: "list",
+        message: "What kind of license should your project have?",
+        name: "license",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        name: "install",
+    },
+    {
+        type: "input",
+        message: "What command should be run to run tests?",
+        name: "test",
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about using the repo?",
+        name: "repoUse",
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about contributing to the repo?",
+        name: "repoContribute",
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile("./output/README.md", data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            console.log(answers)
+            // generateMarkdown(answers)
+        })
+}
 
 // Function call to initialize app
 init();
